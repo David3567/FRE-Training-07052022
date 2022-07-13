05b9a2e003482b193c4b58b6190ba8d0ef5699df
Yue Dong <ydong011@fiu.edu>
2022-07-13 15:30:07 -0500

    update

--- javascript/index.js
+++ javascript/index.js
@@ -186,19 +186,19 @@ console.clear();
 // }
 // foo();
 
-// const myObj = {
+// const myObj = { // MyPromise
 //   pi: 3.14,
-//   foo: function() {
+//   foo: function() { // constructer
 //     console.log('foo: ', this); // <------
 
-//     // function bar() {
-//     //   console.log('bar: ', this);
-//     // }
-//     // const baz = bar.bind(this); // <------
-//     const bar = () => {
+//     function bar() {
 //       console.log('bar: ', this);
 //     }
-//     bar();
+//     // const baz = bar.bind(this); // <------
+//     // const bar = () => {
+//     //   console.log('bar: ', this);
+//     // }
+//     // bar();
 //   },
 //   abc: () => {
 //     console.log('abc: ', this);
@@ -259,7 +259,7 @@ console.clear();
 // fn(2, 3); // over limited
 // fn(2, 3); // over limited
 // fn(2, 3); // over limited
-// const fnone = limitedFunction(5, target); // target, counter = 0 
+// const fnone = limitedFunction(5, target); // target, counter = 0
 // function limitedFunction(num, target){
 //   let counter = 0;
 
@@ -277,10 +277,10 @@ console.clear();
 // const callback2 = (b) => b * 2; // 14
 // const callback3 = (c) => c - 2; // 0
 
-// console.log(runAll(callback1, callback2, callback3)(8)); // 2
+// console.log(runAll(8)(callback1, callback2, callback3)); // 2
 
 // function runAll(...args) {
-  
+
 //   return function(num) {
 //     return args.reduce((acc, cur) => cur(acc), num);
 //     // let res = num;
@@ -301,7 +301,7 @@ console.clear();
 // }
 
 // call stack: [() => console.log(4)],
-// web api, async api: 
+// web api, async api:
 // () => console.log(0) 5s
 // () => console.log(1) 4s
 // () => console.log(2) 3s
@@ -324,38 +324,38 @@ console.clear();
 // 0, 1, 2, 3, 4
 
 //^ ~~~~~~interview question~~~~~~~~~~~~
-const first = [
-    { userid: 2, name: 'Velen' },
-    { userid: 56, name: 'Illidan' },
-    { userid: 23, name: 'Muradin' },
-    { userid: 12, name: 'Sylvanas' },
-    { userid: 44, name: 'Cenarius' },
-    { userid: 4, name: 'Gul\'Dan' }
-];
-const second = [
-    { userid: 2, role: 'Mage' },
-    { userid: 4, role: 'Worlock' },
-    { userid: 56, role: 'Demon Hunter' },
-    { userid: 66, role: 'Druid' },
-    { userid: 87, role: 'Shaman' },
-    { userid: 12, role: 'Hunter' },
-];
-function solution(farr, sarr) {
-  const arr = [...farr, ...sarr];
-  const map = {};
-  // {
-  //   2: { userid: 2, name: 'Velen' },
-  // }
-  arr.forEach(ele => {
-    map[ele.userid] = {
-      ...{userid: null, name: null, role: null},
-      ...map[ele.userid],
-      ...ele,
-    }
-  })
-  return Object.values(map);
-}
-console.log(solution(first, second));
+// const first = [
+//     { userid: 2, name: 'Velen' },
+//     { userid: 56, name: 'Illidan' },
+//     { userid: 23, name: 'Muradin' },
+//     { userid: 12, name: 'Sylvanas' },
+//     { userid: 44, name: 'Cenarius' },
+//     { userid: 4, name: 'Gul\'Dan' }
+// ];
+// const second = [
+//     { userid: 2, role: 'Mage' },
+//     { userid: 4, role: 'Worlock' },
+//     { userid: 56, role: 'Demon Hunter' },
+//     { userid: 66, role: 'Druid' },
+//     { userid: 87, role: 'Shaman' },
+//     { userid: 12, role: 'Hunter' },
+// ];
+// function solution(farr, sarr) {
+//   const arr = [...farr, ...sarr];
+//   const map = {};
+//   // {
+//   //   2: { userid: 2, name: 'Velen' },
+//   // }
+//   arr.forEach(ele => {
+//     map[ele.userid] = {
+//       ...{userid: null, name: null, role: null},
+//       ...map[ele.userid],
+//       ...ele,
+//     }
+//   })
+//   return Object.values(map);
+// }
+// console.log(solution(first, second));
 
 // [
 //   { userid: 2, name: 'Velen', role: 'Mage' },
@@ -367,12 +367,358 @@ console.log(solution(first, second));
 // ]
 
 //* ~~~~~~~~~~~~~~ Day 4 ~~~~~~~~~~~~~~
-//todo: callback function & callback hell
+//~ callback function & callback hell
+// const foo = () => console.log("foo");
+// const getRendomTime = () => Math.floor(Math.random() * 6);
 
-//todo: Promise & MyPromise
+// const callFunInRandomTime = (callback) => {
+// 	const timer = getRendomTime();
+// 	console.log(`wait for ${timer}s`);
 
-//todo: fetch & myFetch
+// 	setTimeout(callback, timer * 1000);
+// };
+// callFunInRandomTime(() => {
+//   callFunInRandomTime(() => {
+//     callFunInRandomTime(() => {
+//       callFunInRandomTime(() => {
+//         callFunInRandomTime(() => {
+//           callFunInRandomTime(() => {
+//             callFunInRandomTime(() => {
+//               callFunInRandomTime(() => {
+//                 callFunInRandomTime(() => {
+//                   callFunInRandomTime(foo);
+//                 });
+//               });
+//             });
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
 
-//* ~~~~~~~~~~~~~~ TodoList ~~~~~~~~~~~~~~
+// const getTodo = (id) => {
+// 	const baseUrl = "https://jsonplaceholder.typicode.com/todos";
+
+// 	return new Promise((resolve, reject) => {
+// 		const xhttp = new XMLHttpRequest();
+// 		xhttp.onreadystatechange = function () {
+// 			if (this.readyState == 4 && this.status == 200) {
+// 				resolve(JSON.parse(xhttp.response));
+// 			}
+// 		};
+// 		xhttp.open("GET", baseUrl + "/" + id);
+// 		xhttp.send();
+// 	});
+// };
+
+// getTodo(12)
+// 	.then((data) => {
+// 		console.log(data);
+// 		return getTodo(22); // 4
+// 	})
+// 	.then((data) => {
+// 		console.log(data);
+// 		return getTodo(32);
+// 	})
+// 	.then((data) => {
+// 		console.log(data);
+// 	});
+
+// //~ async await
+// (async () => {
+//   try {
+//     // const todo12 = await getTodo(12); // 4
+//     // console.log(todo12);
+
+//     // const todo22 = await getTodo(22);  //2
+//     // console.log(todo22);
+
+//     // const todo32 = await getTodo(32); // 3
+//     // console.log(todo32);
+
+//     const arr = [getTodo(12), getTodo(22), getTodo(32)];
+//     const values = await Promise.all(arr);
+
+//     console.log(values);
+
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })(); // 4
+
+//~ Promise & MyPromise
+// class MyPromise {
+// 	thencbqueue = [];
+// 	currentdata = undefined;
+
+// 	constructor(executer) {
+// 		executer(this.resolve.bind(this), this.reject);
+// 	}
+
+// 	resolve(resdata) {
+// 		setTimeout(() => {
+// 			this.currentdata = resdata;
+// 			while (this.thencbqueue.length) {
+// 				const cb = this.thencbqueue.shift();
+// 				this.currentdata = cb(this.currentdata);
+// 			}
+// 		}, 0);
+// 	}
+// 	reject = () => {};
+
+// 	then(thencb) {
+// 		this.thencbqueue.push(thencb);
+// 		return this;
+// 	}
+
+// 	catch() {}
+// }
+
+// new MyPromise((resolve, reject) => {
+// 	console.log("hello");
+// 	resolve(123);
+// })
+// 	.then((data) => {
+// 		console.log(data);
+// 		return 345;
+// 	})
+// 	.then((data12) => console.log(data12));
+
+// console.log("world");
+
+//^ youtube promise
+// const status = {
+// 	PENDING: "pending",
+// 	FULFILLED: "fulfilled",
+// 	REJECTED: "rejected",
+// };
+
+// const isThenable = (maybePromise) =>
+// 	maybePromise && typeof maybePromise.then === "function";
+
+// class MyPromise {
+// 	#status = status.PENDING;
+// 	#value = undefined;
+// 	#reason = undefined;
+// 	#thenQueue = [];
+// 	#finallyQueue = [];
+
+// 	constructor(executor) {
+// 		if (typeof executor === "function") {
+// 			try {
+// 				executor(this.#resolve.bind(this), this.#reject.bind(this));
+// 			} catch (err) {
+// 				this.#reject(err);
+// 			}
+// 		}
+// 	}
+
+// 	#propagationResolved() {
+// 		setTimeout(() => {
+// 			this.#thenQueue.forEach(([controlledPromise, fulfilledFn]) => {
+// 				if (typeof fulfilledFn === "function") {
+// 					const valueOrPromise = fulfilledFn(this.#value);
+
+// 					if (isThenable(valueOrPromise)) {
+// 						valueOrPromise.then(
+// 							(value) => controlledPromise.#resolve(value),
+// 							(reason) => controlledPromise.#reject(reason)
+// 						);
+// 					} else {
+// 						controlledPromise.#resolve(valueOrPromise);
+// 					}
+// 				} else {
+// 					return controlledPromise.#resolve(this.#value);
+// 				}
+// 			});
+
+// 			this.#finallyQueue.forEach(([controlledPromise, sideEffectFn]) => {
+// 				sideEffectFn();
+// 				controlledPromise.#resolve(this.#value);
+// 			});
+
+// 			this.#thenQueue = [];
+// 			this.#finallyQueue = [];
+// 		});
+// 	}
+
+// 	#propagationRejected() {
+// 		setTimeout(() => {
+// 			this.#thenQueue.forEach(([controlledPromise, _, catchFn]) => {
+// 				if (typeof catchFn === "function") {
+// 					const valueOrPromise = catchFn(this.#reason);
+
+// 					if (isThenable(valueOrPromise)) {
+// 						valueOrPromise.then(
+// 							(value) => controlledPromise.#resolve(value),
+// 							(reason) => controlledPromise.#reject(reason)
+// 						);
+// 					} else {
+// 						controlledPromise.#resolve(valueOrPromise);
+// 					}
+// 				} else {
+// 					return controlledPromise.#reject(this.#reason);
+// 				}
+// 			});
+
+// 			this.#finallyQueue.forEach(([controlledPromise, sideEffectFn]) => {
+// 				sideEffectFn();
+// 				controlledPromise.#reject(this.#value);
+// 			});
+
+// 			this.#thenQueue = [];
+// 			this.#finallyQueue = [];
+// 		});
+// 	}
+
+// 	#resolve(value) {
+// 		if (this.#status === status.PENDING) {
+// 			this.#status = status.FULFILLED;
+// 			this.#value = value;
+// 			this.#propagationResolved();
+// 		}
+// 	}
+// 	#reject(reason) {
+// 		if (this.#status === status.PENDING) {
+// 			this.#status = status.REJECTED;
+// 			this.#reason = reason;
+// 			this.#propagationRejected();
+// 		}
+// 	}
+
+// 	then(fulfilledFn, catchFn) {
+// 		const controlledPromise = new MyPromise();
+// 		this.#thenQueue.push([controlledPromise, fulfilledFn, catchFn]);
+
+// 		if (this.#status === status.FULFILLED) {
+// 			this.#propagationResolved();
+// 		} else if (this.#status === status.REJECTED) {
+// 			this.#propagationRejected();
+// 		}
+
+// 		return controlledPromise;
+// 	}
+
+// 	catch(catchFn) {
+// 		return this.then(undefined, catchFn);
+// 	}
+
+// 	finally(sideEffectFn) {
+// 		if (this.#status !== status.PENDING) {
+// 			sideEffectFn();
+
+// 			return this.#status === status.FULFILLED
+// 				? MyPromise.resolve(this.#value)
+// 				: MyPromise.reject(this.#reason);
+// 		}
+
+// 		const controlledPromise = new MyPromise();
+// 		this.#finallyQueue.push([controlledPromise, sideEffectFn]);
+
+// 		return controlledPromise;
+// 	}
+
+// 	//& static functions in the class
+// 	static resolve(resval) {
+// 		return new MyPromise((res, _) => {
+// 			res(resval);
+// 		});
+// 	}
+// 	static reject(rejval) {
+// 		return new MyPromise((_, rej) => {
+// 			rej(rejval);
+// 		});
+// 	}
+//   static all(promiseArr) {
+//     const resolvedArr = new Array(promiseArr.length);
+//     let counter = 0;
+
+//     return new MyPromise((res, rej) => {
+//       promiseArr.forEach((prom, i) => {
+//         if (prom instanceof MyPromise) {
+//           prom.then(data => {
+//             resolvedArr[i] = data;
+//             counter++;
+//             if (counter === promiseArr.length) {
+//               res(resolvedArr);
+//             }
+//           });
+//         } else {
+//           resolvedArr[i] = prom;
+//           counter++;
+//           if (counter === promiseArr.length) {
+//             res(resolvedArr);
+//           }
+//         }
+//       });
+//     });
+//   }
+// }
+
+// const promise1 = MyPromise.resolve(3);
+// const promise2 = 42;
+// const promise3 = new MyPromise((resolve, reject) => {
+// 	setTimeout(resolve, 100, "foo");
+// });
+
+// MyPromise.all([promise1, promise2, promise3]).then((values) => {
+// 	console.log(values);
+// });
+// // expected output: Array [3, 42, "foo"]
+
+//call stack: [then]
+// task queue: 123 => {}
+
+//~ fetch & myFetch
+
+const baseUrl = "https://jsonplaceholder.typicode.com/posts";
+
+const myFetch = (url, options) => {
+	return new Promise((resolve, reject) => {
+		const method = options && options.method ? options.method : "GET";
+
+		const xhttp = new XMLHttpRequest();
+    xhttp.open(method, url);
+
+    if (options && options.headers) {
+      Object.entries(options.headers).forEach(([key, value]) => {
+        xhttp.setRequestHeader(key, value);
+      });
+    }    
+
+		xhttp.onreadystatechange = function () {
+			if (this.readyState === 4 && this.status >= 200 && this.status < 300) {
+				resolve({
+					json() {
+						return JSON.parse(xhttp.response);
+					},
+				});
+			}
+		};
+
+		options && options.body ? xhttp.send(options.body) : xhttp.send();
+	});
+};
+
+myFetch(baseUrl, {
+	method: "POST",
+	body: JSON.stringify({
+		title: "foo",
+		body: "bar",
+		userId: 1,
+	}),
+	headers: {
+		"Content-type": "application/json; charset=UTF-8",
+	},
+})
+	.then((response) => response.json())
+	.then((json) => console.log(json));
+
+// myFetch(baseUrl)
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+//* ~~~~~~~~~~~~~~ Day5 TodoList ~~~~~~~~~~~~~~
 
 //* ~~~~~~~~~~~~~~ Evaluation ~~~~~~~~~~~~~~
