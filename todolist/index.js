fb23a674e0953dd42741b3933271b04f4ddc4b8b
Yue Dong <ydong011@fiu.edu>
2022-07-14 16:11:30 -0500

    update

--- todolist/index.js
+++ todolist/index.js
@@ -1,32 +1,4 @@
-//* ~~~~~~~~~~~~~~~~~~~~~ Api ~~~~~~~~~~~~~~~~~~~~~
-const Api = (() => {
-	// const baseUrl = "https://jsonplaceholder.typicode.com";
-  const baseUrl = 'http://localhost:4232';
-	const path = "todos";
-
-	const getTodos = () =>
-		fetch([baseUrl, path].join("/")).then((response) => response.json());
-
-	const deleteTodo = (id) =>
-		fetch([baseUrl, path, id].join("/"), {
-			method: "DELETE",
-		});
-
-	const addTodo = (newTodo) =>
-		fetch([baseUrl, path].join("/"), {
-			method: "POST",
-			body: JSON.stringify(newTodo),
-			headers: {
-				"Content-type": "application/json; charset=UTF-8",
-			},
-		}).then((response) => response.json());
-
-	return {
-		getTodos,
-		deleteTodo,
-		addTodo,
-	};
-})();
+import { Api } from './api/api.js';
 
 //* ~~~~~~~~~~~~~~~~~~~~~ View ~~~~~~~~~~~~~~~~~~~~~
 const View = (() => {
